2d183f85b9d3ecb7754120f11ceff789562356a2
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-12-28 10:01:00 -0400

    checkForMath

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const cards = document.querySelectorAll('.card');
+let hasFlipperCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!hasFlipperCard) {
+    hasFlipperCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlipperCard = false;
+  checkForMath();
+}
+
+function checkForMath() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
+
+  unflipCards();
 }
 
 cards.forEach(card => {
   card.addEventListener('click', flipCard);
-}) 
+}); 
