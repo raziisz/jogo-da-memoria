ab19eda882fb6f358995bd703db19030952f3b0c
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-12-28 09:48:01 -0400

    efeito rotativo

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const cards = document.querySelectorAll('.card');
 function flipCard() {
   this.classList.toggle('flip');
 }
+
+cards.forEach(card => {
+  card.addEventListener('click', flipCard);
+}) 
